6596ffa6d4193ce0e92f78e622a806f64a774b90
Jose Manuel Cruz Pacheco <cruzpachecojosemanuel@gmail.com>
2024-06-14 13:10:59 +0200

    updated soldier

--- src/main/java/org/example/soldier/Soldier.java
+++ src/main/java/org/example/soldier/Soldier.java
@@ -3,6 +3,7 @@ package org.example.soldier;
 public class Soldier {
     private Integer health;
     private Integer strength;
+    
     public Soldier(int health, int strength) {
         this.health = health;
         this.strength = strength;
