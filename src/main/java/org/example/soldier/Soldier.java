2db24a36dfbf45c77247f762f57fce778acb2491
Jose Manuel Cruz Pacheco <cruzpachecojosemanuel@gmail.com>
2024-06-14 12:19:03 +0200

    added health and strength to soldier

--- src/main/java/org/example/soldier/Soldier.java
+++ src/main/java/org/example/soldier/Soldier.java
@@ -3,5 +3,10 @@ package org.example.soldier;
 public class Soldier {
     private Integer health;
     private Integer strength;
+    public Soldier(int health, int strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+    
 
 }
